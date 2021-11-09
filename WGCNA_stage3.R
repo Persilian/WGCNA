308387f14f8095c279238ba2a324c87fe03ba70d
Persilian <59728330+Persilian@users.noreply.github.com>
2021-11-09 13:41:09 +0100

    Update WGCNA_stage3.R

--- WGCNA_stage3.R
+++ WGCNA_stage3.R
@@ -26,34 +26,32 @@ nCPU = 8
 enableWGCNAThreads(nThreads = nCPU)
 
 ### ------------------------------------------ STAGE III -----------------------------------------------
-### ---------------------------- Adjacency matrix generation (optional) --------------------------------
+### ---------------------------- Adjacency matrix and gene connectivity --------------------------------
 
 ## -------------------------------------------- STEP I -------------------------------------------------
-#For subsequent use with exa.MATRIX.sh and EGAD network analysis, the network adjacency matrix is produced here.
+#Generate the adjacency matrix of your previously built network, for later extractions of sub-networks.
 
 #load expression data from STAGE I
 load(file = "Data/dataInput.RData")
 
 ##calculate adjacency matrix
-#Based on the soft-thresholding choice in STAGE I, we chose the same power again.
+#Based on the soft-thresholding choice in STAGE I, we choose the same power again.
 #CAREFUL - DEFAULT of adjacency() is "unsigned"
 
 adjacency = adjacency(data, type = "unsigned", power = 1)
 save(adjacency, file = "Data/WGCNA_course_Net1_adjacency_matrix.RData")
-#load(file = "Data/WGCNA_course_Net1_matrix.RData")
-write.table(adjacency, "Data/WGCNA_course_Net1_adjacency_matrix.txt", sep = "\t", quote = F)
-
 
 ## ---------------------------------------------- STEP II ----------------------------------------------
-#Compute gene connectivity. For each gene/isoform in the network, the total connections, 
-#within module and outside module connections are calculated. Requires adjacency matrix. 
+#Compute gene connectivity for each gene/isoform in the network. Requires adjacency matrix. 
 
-#load adjacency matrix and network
+#load adjacency matrix if you run this step without creating the adjacency matrix
 #load(file = "Data/WGCNA_course_Net1_adjacency_matrix.RData")
+
+#load network construction data created in WGCNA_stage2.R
 load(file = "Data/WGCNA_course_Net1_construction.RData")
 
-#The function intramodularConnectivity computes the whole network connectivitykTotal,
-#the within module connectivity kWithin, kOut=kTotal-kWithin, andkDiff=kIn-kOut=2*kIN-kTotal
+#The function intramodularConnectivity computes the whole network connectivity kTotal,
+#the within module connectivity kWithin, kOut=kTotal-kWithin, and kDiff=kIn-kOut=2*kIN-kTotal
 connectivity <- intramodularConnectivity(adjacency, net$colors)
 
 write.table(connectivity, "Data/WGCNA_course_Net1_gene_connectivity.txt", sep = "\t", quote = F)
